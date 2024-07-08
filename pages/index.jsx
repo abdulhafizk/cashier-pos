f0194c15ca3f7413581bb4dd90635c2f34b88326
abdulhafizk <hafizh.abdulhafizhkustiawan@gmail.com>
2024-07-08 15:52:55 +0700

    off pretier

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
-    const router = useRouter()
-    useEffect(() => {
-        router.push('dashboard')
-    }, [])
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>HK Mart</title>
-                <meta name="description" content="free cashier app" />
-            </Head>
+  const router = useRouter();
+  useEffect(() => {
+    router.push("dashboard");
+  }, []);
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>HK Mart</title>
+        <meta name="description" content="free cashier app" />
+      </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>Loading dashboard...</h1>
-            </main>
-        </div>
-    )
+      <main className={styles.main}>
+        <h1 className={styles.title}>Loading dashboard...</h1>
+      </main>
+    </div>
+  );
 }
