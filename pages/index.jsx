d25179f7a33f3a933080cd9d29b55315a4723d10
abdulhafizk <132445084+abdulhafizk@users.noreply.github.com>
2024-07-21 18:27:42 +0700

    Shutdown Eslin =

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,9 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import { Spinner } from '@chakra-ui/react'
 
 export default function Home() {
     const router = useRouter()
-    const [progress, setProgress] = useState(0)
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -32,15 +30,44 @@ export default function Home() {
                 <meta name="description" content="free cashier app" />
             </Head>
 
-            <main className={styles.main}>
-                <div className={styles.loadingContainer}>
-                    <h1 className={styles.title}>
-                        Point of Sale Cashier Application
-                    </h1>
-                    <Spinner size="120px" color="red.500" />
-                </div>
-            </main>
-            <Spinner size="120px" color="red.500" />
+            <>
+                {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full">
+        <body class="h-full">
+        ```
+      */}
+                <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+                    <div className="text-center">
+                        <p className="text-base font-semibold text-indigo-600">
+                            404
+                        </p>
+                        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                            Page not found
+                        </h1>
+                        <p className="mt-6 text-base leading-7 text-gray-600">
+                            Sorry, we couldn’t find the page you’re looking for.
+                        </p>
+                        <div className="mt-10 flex items-center justify-center gap-x-6">
+                            <a
+                                href="#"
+                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Go back home
+                            </a>
+                            <a
+                                href="#"
+                                className="text-sm font-semibold text-gray-900"
+                            >
+                                Contact support{' '}
+                                <span aria-hidden="true">&rarr;</span>
+                            </a>
+                        </div>
+                    </div>
+                </main>
+            </>
         </div>
     )
 
